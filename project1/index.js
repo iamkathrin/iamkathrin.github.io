7d24a98ee1c5ed4a2bb1522a6d639875483a1a38
iamkathrin <55264337+iamkathrin@users.noreply.github.com>
2019-10-03 13:28:43 -0700

    commit

--- project1/index.js
+++ project1/index.js
@@ -13,21 +13,48 @@ function plusSlides(n) {
 }
 
 function currentSlide(n){
-  showSlides(slideIndex=n)
+  showSlides(slideIndex=n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName ("mySlides");
+  var slidez = document.getElementsByClassName ("mySlides");
+  var dotz= document.getElementsByClassName("demo");
+  if (n> slidez.length){slideIndex=1}
+  if(n<1) {slideIndex= slidez.length}
+  for(i=0; i< slidez.length; i++){
+    slidez[i].style.display ="none";
+  }
+for (i=0; i< dotz.length; i++){
+  dotz[i].className = dotz[i].className.replace (" active","");
+}
+slidez[slideIndex-1].style.display="block";
+dotz[slideIndex-1].className += " active";
+}
+
+
+var slideindex =1;
+showSlides2(slideindex);
+
+function plusSlides2(n) {
+  showSlides2(slideindex += n);
+}
+
+function currentSlide2(n){
+  showSlides2(slideindex=n);
+}
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName ("mySlides2");
   var dots= document.getElementsByClassName("demo");
-  if (n> slides.length){slideIndex=1}
-  if(n<1) {slideIndex= slides.length}
-  for(i=0, i< slides.length; i++){
+  if (n> slides.length){slideindex=1}
+  if(n<1) {slideindex= slides.length}
+  for(i=0; i< slides.length; i++){
     slides[i].style.display ="none";
   }
 for (i=0; i< dots.length; i++){
   dots[i].className = dots[i].className.replace (" active","");
 }
-slides[slideIndex-1].style.display="block";
-dots[slideIndex-1].className += " active";
+slides[slideindex-1].style.display="block";
+dots[slideindex-1].className += " active";
 }
